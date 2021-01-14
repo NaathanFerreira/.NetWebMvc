7c1a20a517d83456fe7ad420c7795cb9ef8f6f9f
Nathan Ferreira <nathanferreira00@hotmail.com>
2021-01-14 13:55:41 -0300

    SellerService and FindAll

---
+++ Services/SellerService.cs
@@ -0,0 +1,20 @@
+using WebMvc.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace WebMvc.Services
+{
+    public class SellerService
+    {
+        private readonly WebMvcContext _context;
+
+        public SellerService(WebMvcContext context){
+            _context = context;
+        }
+
+        public List<Seller> FindAll(){
+            return _context.Seller.ToList();
+        }
+    }
+}
