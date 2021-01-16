f050f3a85d8e2ce3378d233db8ba69843fe19750
Nathan Ferreira <nathanferreira00@hotmail.com>
2021-01-15 22:50:39 -0300

    Async/Await

--- Services/DepartmentService.cs
+++ Services/DepartmentService.cs
@@ -1,6 +1,8 @@
 using WebMvc.Models;
 using System.Linq;
 using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace WebMvc.Services
 {
@@ -12,8 +14,8 @@ namespace WebMvc.Services
             _context = context;
         }
 
-        public List<Department> FindAll(){
-            return _context.Department.OrderBy(x => x.Name).ToList();
+        public async Task<List<Department>> FindAllAsync(){
+            return await _context.Department.OrderBy(x => x.Name).ToListAsync();
         }
     }
 }
